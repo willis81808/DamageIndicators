178a351358e1bf9b7e77f140f0506506d904946f
willis81808 <willis81808@gmail.com>
2021-06-16 02:06:00 -0600

    initial commit

---
+++ DamageIndicator.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace DamageIndicators
+{
+
+    [RequireComponent(typeof(CanvasGroup))]
+    public class DamageIndicator : MonoBehaviour
+    {
+        private static Dictionary<Transform, float> timeSinceMap = new Dictionary<Transform, float>();
+
+        private CanvasGroup canvasGroup;
+        private TextMeshProUGUI textComponent;
+
+        private Transform target;
+        private string text;
+        private bool healing = false;
+
+        void Awake()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            canvasGroup.alpha = 0;
+            textComponent = new GameObject("Damage Text").AddComponent<TextMeshProUGUI>();
+        }
+
+        void Start()
+        {
+            textComponent.transform.SetParent(transform);
+            textComponent.transform.position = transform.position;
+            textComponent.alignment = TextAlignmentOptions.Center;
+            textComponent.fontSize = 0.75f;
+            textComponent.color = healing ? Color.green : Color.red;
+            textComponent.text = text;
+
+            StartCoroutine(FadeOut());
+        }
+
+        void Update()
+        {
+            transform.position = target.transform.position + Vector3.up;
+        }
+
+        IEnumerator FadeOut()
+        {
+            var startTime = Time.time;
+            var duration = 1f;
+
+            while (Time.time - startTime <= duration)
+            {
+                var progress = (Time.time - startTime) / duration;
+                canvasGroup.alpha = 1 - progress;
+                textComponent.transform.position += Vector3.up * Time.deltaTime;
+                yield return null;
+            }
+
+            canvasGroup.alpha = 0;
+            Destroy(gameObject);
+        }
+
+        public DamageIndicator SetTarget(Transform target)
+        {
+            this.target = target;
+            return this;
+        }
+
+        public DamageIndicator SetText(string text)
+        {
+            this.text = text;
+            return this;
+        }
+
+        public DamageIndicator SetIsHealing(bool healing)
+        {
+            this.healing = healing;
+            return this;
+        }
+    }
+}
---
+++ EntryPoint.cs
@@ -0,0 +1,80 @@
+﻿using BepInEx;
+using UnityEngine;
+using HarmonyLib;
+using UnboundLib;
+
+namespace DamageIndicators
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, "0.0.1")]
+    [BepInProcess("Rounds.exe")]
+    public class EntryPoint : BaseUnityPlugin
+    {
+        private const string ModId = "com.willis.rounds.damageindicators";
+        private const string ModName = "Damage Indicators";
+
+        private static bool ModActive = true;
+        private static Canvas _canvas;
+        private static Canvas canvas
+        {
+            get
+            {
+                if (_canvas == null)
+                {
+                    _canvas = new GameObject("Damage Indicator Canvas").AddComponent<Canvas>();
+                    _canvas.renderMode = RenderMode.WorldSpace;
+                    _canvas.pixelPerfect = false;
+                    DontDestroyOnLoad(_canvas);
+                }
+                return _canvas;
+            }
+        }
+
+        void Awake()
+        {
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+
+        void Start()
+        {
+            Unbound.RegisterGUI(ModName, DrawGUI);
+        }
+
+        void DrawGUI()
+        {
+            ModActive = GUILayout.Toggle(ModActive, "Enabled");
+        }
+
+        public static void SpawnDamageIndicator(Player target, float damage, bool healing = false)
+        {
+            if (!ModActive) return;
+
+            new GameObject("Damage Indicator")
+                .AddComponent<DamageIndicator>()
+                .SetText(damage.ToString("####"))
+                .SetTarget(target.transform)
+                .SetIsHealing(healing)
+                .transform.SetParent(canvas.transform);
+        }
+    }
+    
+    [HarmonyPatch]
+    class Patches
+    {
+        [HarmonyPatch(typeof(CharacterStatModifiers), "DealtDamage")]
+        [HarmonyPostfix]
+        static void DealtDamage_Postfix(Vector2 damage, bool selfDamage, Player damagedPlayer)
+        {
+            if (damagedPlayer == null) return;
+            EntryPoint.SpawnDamageIndicator(damagedPlayer, damage.magnitude);
+        }
+
+        [HarmonyPatch(typeof(HealthHandler), "Heal")]
+        [HarmonyPostfix]
+        static void Heal_Postfix(CharacterData ___data, float healAmount)
+        {
+            EntryPoint.SpawnDamageIndicator(___data.player, healAmount, true);
+        }
+    }
+}
